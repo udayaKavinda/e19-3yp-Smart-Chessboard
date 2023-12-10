3b898fae8deef6be826677f95d3cc502b7a8f0e7
udaya kavinda <e19155@eng.pdn.ac.lk>
2023-12-11 00:39:40 +0530

    initialize node js

---
+++ code/smartchessboard/lib/widgets/custom_textfield.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String hintText;
+  final TextEditingController controller;
+  const CustomTextField(
+      {super.key, required this.hintText, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        fillColor: Colors.orange,
+        filled: true,
+        hintText: hintText,
+      ),
+    );
+  }
+}
