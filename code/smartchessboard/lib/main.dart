9ddfa1a9cbd1a7ec9217e20e9be6937d1c753b9a
Sahira100 <116058976+Sahira100@users.noreply.github.com>
2024-01-06 07:59:30 +0530

    login & signup

--- code/smartchessboard/lib/main.dart
+++ code/smartchessboard/lib/main.dart
@@ -5,10 +5,16 @@ import 'package:smartchessboard/screens/create_room_screen.dart';
 import 'package:smartchessboard/screens/game_screen.dart';
 import 'package:smartchessboard/screens/join_room_screen.dart';
 import 'package:smartchessboard/screens/main_menu_screen.dart';
+import 'package:smartchessboard/screens/login.dart';
+import 'package:smartchessboard/screens/signup.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  //runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -33,6 +39,118 @@ class MyApp extends StatelessWidget {
           CreateRoomScreen.routeName: (context) => const CreateRoomScreen(),
           GameScreen.routeName: (context) => const GameScreen(),
         },
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          // we will give media query height
+          // double.infinity make it big as my parent allows
+          // while MediaQuery make it big as per the screen
+
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            // even space distribution
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Text(
+                    "Welcome",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+
+                    ),
+                    
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text("Experience live chess moves on a real board\nConnect, play, and enjoy the game",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 15,
+
+                  ),)
+                ],
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 2,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/welcome.png")
+                  )
+                ),
+              ),
+
+              Column(
+                children: <Widget>[
+                  // the login button
+                  MaterialButton(
+                    minWidth:250,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+
+                    },
+                    // defining the shape
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        color: Colors.black
+                      ),
+                      borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 15
+                      ),
+                    ),
+                  ),
+                  // creating the signup button
+                  SizedBox(height:20),
+                  MaterialButton(
+                    minWidth: 250,
+                    height: 60,
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=> SignupPage()));
+
+                    },
+                    color: Color(0xff0095FF),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      
+                    ),
+                    child: Text(
+                      "Sign up",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 15
+                      ),
+                    ),
+                  )
+
+                ],
+              )
+
+
+
+            ],
+          ),
+        ),
       ),
     );
   }
